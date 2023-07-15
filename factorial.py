08b911ba1f668160f1821bc135734fac0a211afb
Rakesh Dasary <rakeshdasary@gmail.com>
2023-07-15 15:27:46 -0500

    updated factorial input to 4

--- factorial.py
+++ factorial.py
@@ -9,7 +9,7 @@ def factorial(n):
 #number = int(input("Enter a non-negative integer: "))
 
 # Calculate the factorial
-result = factorial(3)
+result = factorial(4)
 
 # Display the result
-print("The factorial of", 3, "is", result)
+print("The factorial of", 4, "is", result)
